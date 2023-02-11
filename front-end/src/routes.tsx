787d741cbeb0b0f23da3b12f7e2bb0bfd35e3a6a
wellison E Brito <wellysone.brito@gmail.com>
2023-02-11 16:26:48 -0300

    feat: config privates routes

--- front-end/src/routes.tsx
+++ front-end/src/routes.tsx
@@ -1,10 +1,32 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import { Authentication } from './pages/Authentication'
+
+type PrivateRouteProps = {
+  children: JSX.Element
+}
+
+export const PrivateRoute = ({ children }: PrivateRouteProps) => {
+  const isAuthenticated = true
+
+  if (isAuthenticated) {
+    return children
+  }
+
+  return <Navigate to="/auth" />
+}
 
 const RoutesProvider = () => {
   return (
     <Routes>
-      <Route index element={<Authentication />} />
+      <Route path="/auth" element={<Authentication />} />
+      <Route
+        path="/"
+        element={
+          <PrivateRoute>
+            <h1>Teste</h1>
+          </PrivateRoute>
+        }
+      />
     </Routes>
   )
 }
