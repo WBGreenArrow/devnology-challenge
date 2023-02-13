bb6ba6352a7bd786cddc4117ffce91854bf42de2
wellison E Brito <wellysone.brito@gmail.com>
2023-02-12 23:10:20 -0300

    style: refactored SideBar

--- front-end/src/components/SideBar/index.tsx
+++ front-end/src/components/SideBar/index.tsx
@@ -1,35 +1,33 @@
 import { useState, useEffect } from 'react'
-import { Grid, Box } from '@mui/material'
+import { Grid, Box, Tab } from '@mui/material'
 import { useNavigate } from 'react-router-dom'
+import { TabContext, TabList } from '@mui/lab'
 
 import { useStyles } from './styles'
 
-import Tab from '@mui/material/Tab'
-import TabContext from '@mui/lab/TabContext'
-import TabList from '@mui/lab/TabList'
-
 export const SideBar = () => {
-  const [router, setRouter] = useState('')
+  const [router, setRouter] = useState<string>('')
   const navigate = useNavigate()
-  const classes = useStyles()
 
-  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
-    setRouter(newValue)
-  }
+  const classes = useStyles()
 
   useEffect(() => {
     if (router !== '') {
-      navigate(router.toLowerCase())
+      navigate(router)
     }
   }, [router])
+
+  const handleChange = (event: React.SyntheticEvent, routerValeu: string) => {
+    setRouter(routerValeu)
+  }
 
   return (
     <Grid item className={classes.sideBarContainer}>
       <TabContext value={router}>
         <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-          <TabList onChange={handleChange} aria-label="lab API tabs example">
-            <Tab label="Item One" value="Links" />
-            <Tab label="Item Two" value="User" />
+          <TabList orientation="vertical" onChange={handleChange} aria-label="lab API tabs example">
+            <Tab label="Links" value="links" />
+            <Tab label="User" value="user" />
           </TabList>
         </Box>
       </TabContext>
--- front-end/src/pages/Home/index.tsx
+++ front-end/src/pages/Home/index.tsx
@@ -1,30 +1,15 @@
-import { useState } from 'react'
-import {
-  Container,
-  Box,
-  Grid,
-  TextField,
-  FormControl,
-  OutlinedInput,
-  InputAdornment,
-  Button,
-  IconButton,
-} from '@mui/material'
+import { Outlet } from 'react-router-dom'
+import { Container, Box, Grid } from '@mui/material'
 
-import { Link, Outlet } from 'react-router-dom'
-
-import { useStyles } from './styles'
 import { SideBar } from '../../components/SideBar'
+import { useStyles } from './styles'
 
 export const DashBoard = () => {
-  const classes = useStyles()
-
   return (
     <Container maxWidth={'xl'}>
       <Box sx={{ width: '100%', height: '100vh' }}>
         <Grid container sx={{ height: '100%' }}>
           <SideBar />
-          <Link to="/user">Home</Link>
           <Grid item sm>
             <Outlet />
           </Grid>
