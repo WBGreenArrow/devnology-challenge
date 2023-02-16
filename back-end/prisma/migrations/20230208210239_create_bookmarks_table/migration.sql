54b916af19f950a3e87b40f00c83a2b8bdc1e7b2
wellison E Brito <wellysone.brito@gmail.com>
2023-02-16 12:08:24 -0300

    config: prisma migrate push

--- back-end/prisma/migrations/20230208210239_create_bookmarks_table/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Bookmarks" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "label" TEXT NOT NULL,
-    "url" TEXT NOT NULL,
-    "id_user" TEXT NOT NULL,
-    CONSTRAINT "Bookmarks_id_user_fkey" FOREIGN KEY ("id_user") REFERENCES "users" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
-);
